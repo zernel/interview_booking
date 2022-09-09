f45d0a31a62c72f07625b7e883eb551c6fe29c43
Zernel <itzernel@gmail.com>
2022-09-09 10:17:44 +0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+%W(A B C D).each do |i|
+  Investor.create(name: "合伙人 #{i}")
+end
+
+20.times do |i|
+  User.create(name: "创业者 ##{i+1}")
+end
